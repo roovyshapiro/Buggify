c90252a3efdd2b8da736af8922a486fcdef36665
Roovy Shapiro <roovyshapiro@gmail.com>
2019-02-04 15:07:43 -0500

    Initial Commit.

---
+++ Buggify.py
@@ -0,0 +1,107 @@
+##  Buggify is a Python Bugger which automatically inserts random syntax bugs into a program.
+##
+##  Authored by Roovy Shapiro
+##
+##  Here's a basic overview of how the program works:
+##
+##  1. Choose a file and the number of bugs you'd like to introduce.
+##  2. The file is copied to a new file with "BUGGIFIED" appended to the file name before the extension.
+##  3. The number of bugs chosen acts as the range in a for loop
+##      which randomly calls on a list of bug functions to insert into the new copied file.
+##  4. Once completed, a diff is stored showing the differences between the original file and Buggified file.
+##  5. The diff is stored as a new file and renamed with "BUG-ANSWERKEY" appended to the file name before the extension.
+
+
+import shutil, os, difflib, sys, random
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter import Tk
+import buggify_functions as bf
+
+def buggify():
+    '''
+    Prompts user to choose a file.
+    Add 'BUGGIFIED' to a copy of the file before it's extension.
+    Returns the original file and the copy for us to work with.
+    '''
+    #Removes small tk window
+    Tk().withdraw()
+
+    #Prompts User to Choose File.
+    file = askopenfilename()
+
+    #Makes a copy of the original ('filename.py') into the same directory 'filenameBUGGIFIED.py'
+    buggified_file = alter_file_name(file, 'BUGGIFIED')
+    shutil.copy(file, buggified_file)
+
+    return file, buggified_file
+
+def alter_file_name(full_file_path, word_to_insert):
+    '''
+    Returns a filename with a word inserted between a filename and its extension.
+    '''
+    path_of_file = os.path.split(full_file_path)
+    
+    ext_num = len(path_of_file[1]) - path_of_file[1].find('.')
+    ext = path_of_file[1][-ext_num:]
+    filename = path_of_file[1][:-ext_num]
+    
+    new_filename = filename + word_to_insert + ext
+    return new_filename
+
+
+def convert_to_list(file):
+    '''
+    Returns the new BUGGIFIED file we created as a list.
+    '''
+    with open(file) as f:
+        filelist = f.read().splitlines()
+    return filelist
+
+
+def write_changes(file, filechanges):
+    '''
+    Writes the changes we made to our file.
+    '''
+    with open(file, 'w') as f:
+        f.write(filechanges)
+    return file
+
+def diff_output(file, bugfile, answers):
+    '''
+    Returns a diff file between the original file and the buggified file.
+    '''
+    with open(file) as f1:
+        f1_txt = f1.read().splitlines()
+        with open(bugfile) as f2:
+            f2_txt = f2.read().splitlines()
+            diff = difflib.unified_diff(
+                f1_txt,
+                f2_txt,
+                fromfile='f1.txt',
+                tofile='f2.txt',
+                n = 0,
+                )
+    with open(answers,'w') as f:
+        for line in diff:
+            f.write(line)
+            f.write('\n')
+    return answers
+
+
+
+original_file, copy_of_file = buggify()
+filelist = convert_to_list(copy_of_file)
+answer_key = alter_file_name(original_file, 'BUG-ANSWERKEY')
+
+num_bugs = 50
+while num_bugs > 1:
+    random_num = random.randint(0,len(bf.function_list) - 1)
+    filelist, num_bugs = bf.function_list[random_num](filelist, num_bugs)
+    
+filechanges = '\n'.join(filelist)
+
+finalbugs = write_changes(copy_of_file, filechanges)
+diff_output(original_file, copy_of_file, answer_key)
+
+
+
---
+++ buggify_functions.py
@@ -0,0 +1,175 @@
+import random
+
+def random_line(filelist):
+    '''
+    Gets a random non commented line from the file as a string
+    and returns it as a list of charachters.
+    '''
+    line_list = []
+    while not line_list: #make sure not to get an empty line
+        try: #in case its zero
+            random_num = random.randint(0, len(filelist) - 1)
+        except ValueError:
+            random_num = 0
+        if (not filelist[random_num].strip().startswith('#') and
+            not filelist[random_num].strip().startswith("'''") and
+            not filelist[random_num].strip().startswith('"""')):
+            line_list = list(filelist[random_num])
+    return random_num, line_list
+
+
+def single_char_swap(filelist, num_bugs, char1, char2):
+    '''
+    Randomly swaps two supplied charachters for each other.
+    '''
+    mylist = []
+    random_num, mylist = random_line(filelist)
+    mylist = filelist[random_num].split(' ')
+    for x in range(len(mylist)):
+        randomizer = random.randint(0,1)   #not too many on same line
+        if randomizer:
+            if char1 in mylist[x]:
+                mylist[x] = mylist[x].replace(char1, char2)
+                num_bugs -=1
+                break
+            elif char2 in mylist[x]:
+                mylist[x] = mylist[x].replace(char2, char1)
+                num_bugs -=1
+                break
+            else:
+                continue
+        elif randomizer == 0:
+            continue        
+    filelist[random_num] = ' '.join(mylist)
+    return filelist, num_bugs
+
+def tabs_spaces(filelist, num_bugs):
+    '''
+    Randomly adds or subtracts a space in a tabbed line,
+    or adds a tab or double tab where it doesn't belong.
+    '''
+    mylist = []
+    random_num, mylist = random_line(filelist)
+    flag = random.randint(0,1)
+    if mylist[:4] == [' ', ' ', ' ', ' ']:
+        if flag:
+            mylist.insert(0, ' ')
+        else:
+            del mylist[0]
+    elif flag:
+        for x in range(4):
+            mylist.insert(0, ' ')
+    else:
+        for x in range(8):
+            mylist.insert(0, ' ')
+
+    num_bugs -=1                      
+    filelist[random_num] = ''.join(mylist)
+    return filelist, num_bugs
+
+def num_change(filelist, num_bugs):
+    '''
+    Randomly adds or subtracts an int by 1.
+    '''
+    mylist = []
+    random_num, mylist = random_line(filelist)
+    for x in range(len(mylist)):
+        randomizer = random.randint(0,2)
+        if mylist[x].isdigit() and randomizer != 2:
+            mylist[x] = int(mylist[x])
+            if randomizer:
+                mylist[x] += 1
+            elif randomizer == 0:
+                mylist[x] -= 1
+            num_bugs -= 1
+            mylist[x] = str(mylist[x])
+            break
+        else:
+            continue
+    filelist[random_num] = ''.join(mylist)
+    return filelist, num_bugs
+
+
+def period_swap(filelist, num_bugs):
+    '''
+    Randomly change period to a comma and vice versae.
+    '''
+    new_filelist, num_bugs_update = single_char_swap(filelist, num_bugs, ',', '.')
+    return new_filelist, num_bugs_update
+    
+
+def zero_o(filelist, num_bugs):
+    '''
+    Randomly change 'o' to a Zero and vice versae.
+    '''
+    new_filelist, num_bugs_update = single_char_swap(filelist, num_bugs, 'o', '0')
+    return new_filelist, num_bugs_update
+
+def elif_else(filelist, num_bugs):
+    '''
+    Randomly change 'elif' to 'else' and vice versae.
+    '''
+    new_filelist, num_bugs_update = single_char_swap(filelist, num_bugs, 'elif', 'else')
+    return new_filelist, num_bugs_update
+
+def parentheses(filelist, num_bugs):
+    '''
+    Randomly changes brackets, parentheses and curly braces.
+    '''
+    bracks_open = ['(','{','[',':',]
+    bracks_closed = [')','}',']',';',':']
+    randomizer = random.randint(0,1)
+    if randomizer:
+        new_filelist, num_bugs_update = single_char_swap(filelist, num_bugs,random.choice(bracks_open), random.choice(bracks_open))
+    else:
+        new_filelist, num_bugs_update = single_char_swap(filelist, num_bugs,random.choice(bracks_closed), random.choice(bracks_closed))
+
+    return new_filelist, num_bugs_update
+
+def bugged_comment(filelist, num_bugs):
+    '''Replaces comment with a specified comment.
+    '''
+    bug_comment = '#THIS COMMENT HAS BEEN BUGGIFIED!'
+    flag = True
+    while flag:
+        for line in range(len(filelist)):
+            #Sometimes this function will buggify a buggified docstring. need to review
+            if filelist[line].strip().startswith('#') and random.randint(1, 5) == 1:
+                filelist[line] = bug_comment
+                flag = False
+                num_bugs -= 1
+                break
+            else:
+                continue
+    return filelist, num_bugs
+
+def bugged_docstring(filelist, num_bugs):
+    '''
+    Randomly replaces pre-defined text within triple quotes.
+    '''
+    doc_list = []
+    for line in range(len(filelist)):
+        if filelist[line].strip().startswith("'''") or filelist[line].strip().startswith('"""'):
+            doc_list.append(line)
+
+    if len(doc_list) == 0 or len(doc_list) % 2 != 0:  
+        return filelist, num_bugs
+    start_quotes = [x for x in doc_list if doc_list.index(x) % 2 == 0]
+    end_quotes = [x for x in doc_list if doc_list.index(x) % 2 == 1]
+    random_num = (random.randint(0, len(start_quotes))) - 1
+    for y in range(start_quotes[random_num], (end_quotes[random_num]) +1):
+        filelist[y] = '#BUGGIFIED DOCSTRING'.rjust(24)
+    num_bugs -= 1
+    return filelist, num_bugs
+
+#List of all the bugs which buggify randomly chooses from to implement.    
+function_list = [
+                 zero_o,
+                 parentheses,
+                 bugged_comment,
+                 bugged_docstring,
+                 period_swap,
+                 tabs_spaces,
+                 num_change,
+                 elif_else
+                 ]
